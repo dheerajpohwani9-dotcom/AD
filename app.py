a3456d1a42b1d6de9536f0f0fed01d86e788a596
dheerajpohwani9-dotcom <dheerajpohwani9@gmail.com>
2025-12-30 04:58:09 -0800

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import datetime
 st.set_page_config(page_title="Z&J ka Chatbot", layout="centered")
 
 GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
-PDF_PATH = "data/Zeeshan_Chatbot_Company_Manual.pdf"
+PDF_PATH = "Zeeshan_Chatbot_Company_Manual.pdf"
 MODEL_NAME = "llama-3.1-8b-instant"
 
 
@@ -158,5 +158,6 @@ if user_input:
         with st.spinner("Soch raha hoon..."):
             answer = get_answer(user_input, st.session_state.messages)
         st.markdown(answer)
+
 
     st.session_state.messages.append({"role": "assistant", "content": answer})
